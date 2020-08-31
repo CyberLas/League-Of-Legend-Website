5f6270ecfe048170a3895490e8cc5aae61ae4582
Carlos Angeles <33170529+CyberLas@users.noreply.github.com>
2020-08-31 02:03:24 -0500

    Cambiando algunas todo y arreglando el index y trattando de ver la parte principal de la pagina web

--- js/main-js.js
+++ js/main-js.js
@@ -1,8 +1,44 @@
+var a=0;
+function openNav() {
+    if(a==0)
+    {
+        document.getElementById("mySidepanel").style.width = "250px";
+        a=1;
+    }else{
+        if(a==1)
+        {
+            document.getElementById("mySidepanel").style.width = "0";
+            a=0;
+        }
+    }
+}
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
 
 
-$( "#a" ).load( "data.txt" );
+/** load page  */
+$('#indice1').on('click', function () {
+    $( "#container-id" ).load("view-index/windows-1.html");
+});
 
-/*
+/** load page  */
+$('#indice2').on('click', function () {
+    $( "#container-id" ).load("view-index/windows-2.html");
+});
+
+/** load page  */
+$('#indice4').on('click', function () {
+    $( "#container-id" ).load("view-index/windows-9.html");
+});
+
+/** load page  */
+$('#indice5').on('click', function () {
+    $( "#container-id" ).load("view-index/windows-10.html");
+});
+
+
+/*$( "#a" ).load("windows-gare.html");
 document.getElementById("iron").innerHTML = Math.floor(Math.random() * (60 - 10) + 10)+'%';
 document.getElementById("bronze").innerHTML = Math.floor(Math.random() * (60 - 20) + 10)+'%';
 document.getElementById("silver").innerHTML = Math.floor(Math.random() * (60 - 30) + 10)+'%';
